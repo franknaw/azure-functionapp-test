8b1dfbbdcfc1bfd1555a95788eadf4baa81b08e4
Franknaw <franknaw@gmail.com>
2025-05-01 08:12:17 -0400

    Update function_app.py

--- function_app.py
+++ function_app.py
@@ -6,7 +6,7 @@ app = func.FunctionApp()
 
 
 @app.function_name("api_hello_world")
-@app.route(route="hello", methods=["GET"], auth_level=func.AuthLevel.ANONYMOUS)
+@app.route(route="hello", methods=["POST"], auth_level=func.AuthLevel.ANONYMOUS)
 def api_hello_world(req: func.HttpRequest) -> func.HttpResponse:
     """
     Simple Hello World endpoint.
